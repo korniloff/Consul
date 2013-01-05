7b0cc8189ea98ba708b876768844d3ff5ced4a95
Egor Sergeev <pu88.mail.ru>
2013-01-06 00:00:18 +0200

    Правки формы

--- inc/contactform.php
+++ inc/contactform.php
@@ -1,51 +1,50 @@
 <script language = JavaScript>
 function send()
 {
-str  =  contform.clientname.value;
-   if (encodeURI(str).length  <  1)
+var strName  =  contform.clientname.value;
+   if (encodeURI(strName).length  <  1)
    {
      alert("<?=Translate($LANG,'Введите, пожалуйста, Ваше имя');?>!");
      contform.clientname.focus();
      return  false;
    }
-str  =  contform.clientmail.value;
-   if (encodeURI(str).length  <  1)
+var strMail  =  contform.clientmail.value;
+   if (encodeURI(strMail).length  <  1)
    {
      alert("<?=Translate($LANG,'Введите, пожалуйста, E-Mail');?>!");
      contform.clientmail.focus();
      return  false;
    }
-str  =  contform.clientcountry.value;
-   if (encodeURI(str).length  <  1)
+var strCountry  =  contform.clientcountry.value;
+   if (encodeURI(strCountry).length  <  1)
    {
      alert("<?=Translate($LANG,'Введите, пожалуйста, страну');?>!");
      contform.clientcountry.focus();
      return  false;
    }
-str  =  contform.clientphone.value;
-   if (encodeURI(str).length  <  1)
+var strPhone  =  contform.clientphone.value;
+   if (encodeURI(strPhone).length  <  1)
    {
      alert("<?=Translate($LANG,'Введите, пожалуйста, телефон!');?>");
      contform.clientphone.focus();
      return  false;
    }
-str  =  contform.message.value;
-   if (encodeURI(str).length  <  1)
+var strMessage  =  contform.message.value;
+   if (encodeURI(strMessage).length  <  1)
    {
-     alert("Введите, пожалуйста, сообщение!');?>");
+     alert("<?=Translate($LANG,'Введите, пожалуйста, сообщение!');?>");
      contform.message.focus();
      return  false;
    }
 
  
-var data = 'Господин '+ $('#clientname').val()+' E-mail: '+
-            $('#clientmail').val() + ' Страна: '+$('#clientcountry').val()+
-            'Телефон: '+ $('#clientphone').val()+ 'Сообщает :'+$('#message').val();
+var   data = 'clientname='+ strName+'&email='+strMail+'&clientcountry='+strCountry+'&clientphone='+strPhone+'&message='+strMessage;
+//      alert(data);
   // Отсылаем паметры
        $.ajax({
                 type: "POST",
                 url: "senddata.php",
-                data: "data="+data,
+                data: data, 
                 // Выводим то что вернул PHP
                 success: function(html) {
  //предварительно очищаем нужный элемент страницы
--- senddata.php
+++ senddata.php
@@ -1,19 +1,31 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
-<head>
-
-   <meta http-equiv="Content-Type" content="text/html; charset=windows-1251"/>
-
-</head>
-<BODY>   
 <?php
-include("inc/settings.php");
+//include("inc/settings.php");
+setlocale(LC_CTYPE, 'ru_RU.CP1251');
 include ("inc/head.php");
+include("admin/inc/const.php");
+require_once("inc/functions.php");
 
 //Получаем данные
-$data = $_POST[data];
-mailSend($adminemail,"Consul site info", $data);
+//$data = $_POST[data];
+//                data: "clientname="+ $('#clientname').val()+"&email="$('#clientmail').val()+"&clientcountry="+$('#clientcountry').val()+
+//                      "&clientphone"+$('#clientphone').val()+"&message="+$('#message').val();
+$clientname=iconv("utf-8", "windows-1251", $_POST['clientname']);
+$country=iconv("utf-8", "windows-1251", $_POST['clientcountry']);
+$message=iconv("utf-8", "windows-1251", $_POST['message']);
+$TEXT.="Сообщение от сайта ".$mainurl." ".date("d/m/Y H:i")."<br><br>";
+$TEXT.="<b>Посетитель:</b> ".$clientname;
+$TEXT.="<br><b>Email:</b> ".$_POST['email'];
+$TEXT.="<br><b>Страна:</b> ".$country;
+$TEXT.="<br><b>Телефон:</b> ".$_POST['clientphone'];
+$TEXT.="<br><br><b>Сообщение:</b> ".$message;
+
+$nnn=$_GET['clientname'];
+$mmm=$_GET['email'];
+
+mailSend($adminemail,"Сообщение от сайта Consul",$TEXT,$from="$mmm",$replyto="$mmm",$bcc=""); 
+//mailSend($adminemail,"Consul site info", $data);
 ?>
+<BODY>
 <center> <h2><?=Translate($LANG,'Сообщение успешно отправлено');?>!</h2>
 <p><?=Translate($LANG,'Благодарим за интерес к нашим предложениям');?>!
 </p>
